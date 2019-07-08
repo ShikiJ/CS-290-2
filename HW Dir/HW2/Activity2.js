ad745c477bf9fd8f181f182f5545096c85cc1aec
SolonJones <imisuzu0@hotmail.com>
2019-07-08 10:41:26 +0800

    HW1 and additional HW!

---
+++ HW Dir/HW2/Activity2.js
@@ -0,0 +1,16 @@
+print();
+//func();
+//if call func() here an error will appear! 
+
+var func = function()
+{
+	console.log("This is variable-form");
+};
+
+func();
+
+function print()
+{
+	console.log("this is function-form");
+}
+
---
+++ HW Dir/HW2/Activity3.js
@@ -0,0 +1,23 @@
+let obj1 = {a:2,b:2,c:3};
+let obj2 ={d:2,b:2,c:3};
+
+let deepEqual = function(obj1,obj2)
+{
+	if(typeof(obj1) !=typeof(obj2))
+		return false; 
+	var final = true; 
+	for(var prop in obj1)
+	{
+	//	for(var prop2 in obj2)
+	//	console.log("obj.prop is " + obj1.prop);
+	//	console.log("obj[prop] is " + obj1[prop]);
+			if(obj1[prop] !== obj2[prop])
+			{
+				final =false; 
+			}
+	//	}
+
+	}
+	return final;
+};
+console.log(deepEqual(obj1,obj2));
